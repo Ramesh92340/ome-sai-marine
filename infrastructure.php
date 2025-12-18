e852cb2148025041fb90b255348ccf5a54d6f10f
Ramesh92340 <rameshpilli1428@gmail.com>
2025-12-18 23:26:43 +0530

    services and infrastructure pages  updated

---
+++ infrastructure.php
@@ -0,0 +1,97 @@
+<?php include 'navbar.php';  ?>
+
+
+
+
+
+
+<main id="main">
+
+
+
+    <section class="services-header-section py-5">
+        <div class="container" data-aos="fade-up">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-md-2 col-lg-4 d-none d-md-block text-md-end pe-md-5">
+                    <div class="services-icon-box">
+                        <img src="assets/img/marine/serices_kit.png" alt="Icon" class="img-fluid" style="width: 100px;">
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-8">
+                    <h2 class="services-main-title">Our Infrastructure</h2>
+                    <p class="services-main-desc">
+                        We believe strong infrastructure is the foundation of reliable and efficient shipbuilding.
+                        Ome Sai Marine is equipped with state-of-the-art facilities to handle diverse marine projects.
+                    </p>
+                    <div class="mt-4">
+                        <a href="#contact" class="  about-btn">Get In Touch</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="facilities-section py-5">
+        <div class="container">
+
+            <div class="row align-items-center mb-5">
+                <div class="col-lg-6 mb-4 mb-lg-0">
+                    <img src="assets/img/marine/fabrication_workshops.png" alt="Fabrication Workshops" class="img-fluid rounded shadow-sm">
+                </div>
+                <div class="col-lg-6">
+                    <div class="facility-text-block rounded-right-c p-5">
+                        <h3 class="facility-title">Fabrication Workshops</h3>
+                        <p class="facility-text">Modern Workshops With Heavy-Duty Equipment For Precision Fabrication And Vessel Assembly.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-items-center flex-row-reverse mb-5">
+                <div class="col-lg-6 mb-4 mb-lg-0">
+                    <img src="assets/img/marine/dry_dock_facilities.png" alt="Dry Dock Facilities" class="img-fluid rounded shadow-sm">
+                </div>
+                <div class="col-lg-6">
+                    <div class="facility-text-block rounded-left-c p-5">
+                        <h3 class="facility-title">Dry Dock Facilities</h3>
+                        <p class="facility-text">Well-Equipped Docking Areas For Inspections, Retrofitting, And Vessel Repairs.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-items-center mb-5">
+                <div class="col-lg-6 mb-4 mb-lg-0">
+                    <img src="assets/img/marine/quality_control.png" alt="Quality Control Systems" class="img-fluid rounded shadow-sm">
+                </div>
+                <div class="col-lg-6">
+                    <div class="facility-text-block rounded-right-c p-5">
+                        <h3 class="facility-title">Quality Control Systems</h3>
+                        <p class="facility-text">Well-Equipped Docking Areas For Inspections, Retrofitting, And Vessel Repairs.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-items-center flex-row-reverse">
+                <div class="col-lg-6 mb-4 mb-lg-0">
+                    <img src="assets/img/marine/skilled_workforce.png" alt="Skilled Workforce" class="img-fluid rounded shadow-sm">
+                </div>
+                <div class="col-lg-6">
+                    <div class="facility-text-block rounded-left-c p-5">
+                        <h3 class="facility-title">Skilled Workforce</h3>
+                        <p class="facility-text">A Team Of Engineers, Technicians, And Craftsmen Trained To Meet The Latest Marine Industry Standards.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+
+
+ 
+</main>
+
+
+<?php include 'footer.php'; ?>
---
+++ services.php
@@ -0,0 +1,128 @@
+<?php include 'navbar.php';  ?>
+
+
+
+
+
+
+<main id="main">
+
+
+
+    <section class="services-header-section py-5">
+        <div class="container" data-aos="fade-up">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-md-4 d-none d-md-block text-md-end pe-md-5">
+                    <div class="services-icon-box">
+                        <img src="assets/img/marine/serices_kit.png" alt="Icon" class="img-fluid" style="width: 100px;">
+                    </div>
+                </div>
+
+                <div class="col-md-8">
+                    <h2 class="services-main-title">Our <br>Marine Services</h2>
+                    <p class="services-main-desc">
+                        At Ome Sai Marine, we provide a comprehensive range of marine services designed to
+                        support every stage of vessel construction, maintenance, and operation.
+                    </p>
+                    <div class="mt-4">
+                        <a href="#contact" class="  about-btn">Get In Touch</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="services-list-section pb-5">
+        <div class="container">
+
+            <div class="row align-items-center service-row" data-aos="fade-right">
+                <div class="col-lg-7">
+                    <img src="assets/img/marine/ship_repairs.png" class="img-fluid service-img" alt="Ship Repairs">
+                </div>
+                <div class="col-lg-5">
+                    <div class="service-text-card card-right">
+                        <h3 class="service-title">Ship Repairs</h3>
+                        <p class="service-para">
+                            We provide complete repair solutions for barges, dredgers, offshore vessels, and cargo ships. Our expert team ensures cost-effective and
+                            timely steel repairs, machinery upgrades, and system installations.
+                        </p>
+                        <div class="service-accent-line"></div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="row align-items-center flex-row-reverse service-row" data-aos="fade-left">
+                <div class="col-lg-7">
+                    <img src="assets/img/marine/fishing_vessels.png" class="img-fluid service-img" alt="Ferry Vessels">
+                </div>
+                <div class="col-lg-5">
+                    <div class="service-text-card card-left">
+                        <h3 class="service-title">Ferry &
+                            Fishing Vessels</h3>
+                        <p class="service-para">
+                            Designing and building ferries, river cruises, and fishing boats with high safety standards and class certifications.
+                            We customize vessels for passenger comfort and fishing requirements.
+                        </p>
+                        <div class="service-accent-line"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-items-center service-row" data-aos="fade-right">
+                <div class="col-lg-7">
+                    <img src="assets/img/marine/tug_bots.png" class="img-fluid service-img" alt="Tug Boats">
+                </div>
+                <div class="col-lg-5">
+                    <div class="service-text-card card-right">
+                        <h3 class="service-title">Tug Boats</h3>
+                        <p class="service-para">
+                            Construction of powerful and reliable tugboats designed for port operations, offshore support,
+                            and utility use. Built for strength, safety, and performance.
+                        </p>
+                        <div class="service-accent-line"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-items-center flex-row-reverse service-row" data-aos="fade-left">
+                <div class="col-lg-7">
+                    <img src="assets/img/marine/tankers.png" class="img-fluid service-img" alt="Pontoon">
+                </div>
+                <div class="col-lg-5">
+                    <div class="service-text-card card-left">
+                        <h3 class="service-title">Mini Bulkers & Tankers</h3>
+                        <p class="service-para">
+                            Designing and building ferries, river cruises, and fishing boats with high safety standards and class certifications.
+                            We customize vessels for passenger comfort and fishing requirements.
+                        </p>
+                        <div class="service-accent-line"></div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row align-items-center service-row" data-aos="fade-right">
+                <div class="col-lg-7">
+                    <img src="assets/img/marine/pantoon.png" class="img-fluid service-img" alt="Tug Boats">
+                </div>
+                <div class="col-lg-5">
+                    <div class="service-text-card card-right">
+                        <h3 class="service-title">Flat Top Pontoon</h3>
+                        <p class="service-para">
+                            We build strong and stable flat top pontoons designed for heavy loads, offshore support, and marine construction.
+                            Each pontoon is durable, well-balanced, and built to class-approved safety standards.
+                        </p>
+                        <div class="service-accent-line"></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+</main>
+
+
+<?php include 'footer.php'; ?>
